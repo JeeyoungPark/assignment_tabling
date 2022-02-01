89267941e2558fee5fcd808eec9e29ffbf21b37b
jeeyoung_park <jeeyoung0321@gmail.com>
2022-02-01 15:44:32 +0900

    style: markup Detail component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import '~/scss/style.scss';
 import Header from './components/Header';
 import List from './components/List';
+import Detail from './components/Detail';
 import { request } from './utils/api';
 
 export default function App({ $target }) {
   this.state = {
     reservations: [],
+    clickedId: '',
+    clickedReservation: {},
   };
 
   const header = new Header({ $target });
@@ -15,20 +18,31 @@ export default function App({ $target }) {
     initialState: this.state.reservations,
   });
 
+  const detail = new Detail({
+    $target,
+    initialState: this.state.clickedReservation,
+  });
+
   this.setState = nextState => {
     this.state = nextState;
 
     list.setState({
       reservations: this.state.reservations,
     });
+
+    detail.setState({
+      clickedReservation: this.state.clickedReservation,
+    });
   };
 
   const getReservations = async () => {
     const { reservations } = await request('/v1/store/9533/reservations');
+    const firstReservation = reservations[0];
 
     this.setState({
       ...this.state,
       reservations,
+      clickedReservation: firstReservation,
     });
   };
 
---
+++ src/components/Detail.js
@@ -0,0 +1,58 @@
+export default function Detail({ $target, initialState = {} }) {
+  const $aside = document.createElement('aside');
+
+  $aside.className = 'detail-wrapper';
+  $target.appendChild($aside);
+
+  this.state = initialState;
+
+  this.setState = nextState => {
+    if (this.state.clickedReservation !== nextState.clickedReservation) {
+      this.state = nextState;
+      this.render();
+    }
+  };
+
+  this.render = () => {
+    const reservationTemplate = data => {
+      return `
+        <div>예약 상태</div>
+        <div>${
+          !data?.status ? '' : data.status === 'seated' ? '착석' : '예약 중'
+        }</div>
+        <div>예약 시간</div>
+        <div>${!data?.timeReserved ? '' : data.timeReserved}</div>
+        <div>접수 시간</div>
+        <div>${!data?.timeRegistered ? '' : data.timeRegistered}</div>
+      `;
+    };
+
+    const customerTemplate = data => {
+      return `
+        <div>고객 성명</div>
+        <div>${!data ? '' : data.name}</div>
+        <div>고객 등급</div>
+        <div>${!data ? '' : data.level}</div>
+        <div>고객 메모</div>
+        <div>${!data ? '' : data.memo}</div>
+        <div>요청 사항</div>
+        <div>${!data ? '' : data.request}</div>
+      `;
+    };
+
+    $aside.innerHTML = `
+      <div class="detail">
+        <div class="detail-reservation">
+          <h3>예약 정보</h3>
+          ${reservationTemplate(this.state.clickedReservation)}
+        </div>
+        <div class="detail-customer">
+          <h3>고객 정보</h3>
+          ${customerTemplate(this.state.clickedReservation?.customer)}
+        </div>
+      </div>
+    `;
+  };
+
+  this.render();
+}
